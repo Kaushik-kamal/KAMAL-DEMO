c464c3a02b51bab7f3ee3a35f4750319d1f50ebd
Kaushik-kamal <sharma.k2k05@gmail.com>
2025-01-15 17:18:44 +0530

    avg of numbers

---
+++ avg_of_4nnumbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,sum,avg;
+    printf("enter a,b,c,d");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    sum= a+b+c+d;
+    avg=sum/4;
+    printf("the average of 4 number is %d",avg);
+
+
+}
+
+
