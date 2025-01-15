3d90353d30e2bdf9dc6ac4408a10a1cb09b6c133
Kamal09052006 <sharma.k2k05@gmail.com>
2025-01-15 15:00:17 +0530

    area of circle

---
+++ area_of_circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int r;
+    printf("enter r :");
+    scanf("%d",&r);
+    float area = 4*3.14*r*r;
+    printf("area of square is %f",area);
+    return 0;
+}
